09917b8f5f407f02e69ab2697fadf867f879a14f
Richard Taylor <moomerman@gmail.com>
2024-09-17 09:46:05 +0100

    Release v0.3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v0.3.1
+
+  * Add B2B SSO Authenticate endpoint (thanks @mbenatti)
+
 ## v0.3.0
 
   * Add B2B endpoints (thanks @trashhalo)
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Stytch.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/moomerman/stytch-elixir"
-  @version "0.3.0"
+  @version "0.3.1"
 
   def project do
     [
